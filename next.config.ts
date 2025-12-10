78533d2f41153e04d5b63f5d31bbb4a753e165d0
rorado <sohaybahrich3@gmail.com>
2025-12-10 23:26:00 +0100

    upgrade

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
-import {NextConfig} from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
- 
-const nextConfig: NextConfig = {
-    eslint: {
-    // Warning: this allows production builds to succeed even with ESLint errors
+import createNextIntlPlugin from "next-intl/plugin";
+
+const nextConfig: any = {
+  eslint: {
     ignoreDuringBuilds: true
   }
 };
- 
+
 const withNextIntl = createNextIntlPlugin();
+
 export default withNextIntl(nextConfig);
